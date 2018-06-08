f692c8b50e11e671ae21ecfd2fe84c39e188d812
Andrey Gaichikov <gaichikov@gmail.com>
2018-06-08 20:44:59 +0000

    Refactored the code

--- raspi_daemon.py
+++ raspi_daemon.py
@@ -23,32 +23,44 @@ logging.basicConfig(level=logging.DEBUG,
 
 class Raspi(object):
     def __init__(self):
-        self.asterisk_check_int = 5 
-        self.asterisk_status_last = datetime.now()
+        self.check_int = 5 
+        self.last_check_ts = datetime.now()
 
 
 class Channel(object):
     ''' Controls channels statuses '''
     def __init__(self, channel_id):
         self.id = channel_id
-        self.channel_name = 'channel'+channel_id
-        self.pairing_status = False
-        self.channel_status = True
-        self.channel_blocked = False
-        self.channel_active_int = 900 # Seconds
-        self.channel_block_int = 600  # Seconds
+        self.channel_name = 'channel' + str(channel_id)
+
+        self.pairing_status = None
+        self.channel_status = None
+        self.route_status = None
+        self.workinghours_status = None
+
+        # self.channel_block_int = 600  # Seconds
+        
         self.total_call_time = 0  # Total duration to call before blocking
-        self.workinghours = [7, 19]
-        self.workinghours_status = False
-        self.last_check_ts = datetime.now() # last periodical check ts
-        self.channel_ts_last = datetime.now() + timedelta(seconds=random.randint(60, 90))  # Start count from different timestamps
+        self.workinghours = [7, 19] # GMT
+        
+        self.last_busy_period = 0
+        self.last_free_period = 0
+
+        self.idle_period = random.randint(50, 600) # Interval to wait between calls
+
+        # self.last_check_ts = datetime.now() # last periodical check ts
+        
         self.download_int = random.randint(10,15)  # period between downloads
         self.last_downloads = datetime.now()
 
 
 raspi = Raspi()
 channels = []
-for channel_id in range(1,5):
+
+channels_amount = len(subprocess.getoutput('asterisk -x "mobile show devices"').split('\n')[1:])
+print('Total configured channels ' + str(channels_amount))
+
+for channel_id in range(1,channels_amount+1):
     channel = Channel(channel_id)
     print(channel.__dict__)
     channels.append(channel)
@@ -67,32 +79,35 @@ def main():
     add_routes(sites)
 
     while True:
-        if (datetime.now() - raspi.asterisk_status_last).total_seconds() > raspi.asterisk_check_int:
-            check_asterisk_mobile()
+        # Set workinghours status
+        check_workinghours()
+        # Checking actual channel status - paired/not paired, no service
+        check_channels()
+        # Check current routes , blocked/unblocked
+        check_routes()
+
+        raspi.last_check_ts = datetime.now()
 
         for channel in channels:
-            if channel.pairing_status:
-                if (channel.workinghours[0] <= int(datetime.now().strftime('%H')) < channel.workinghours[1]):
-                    channel.workinghours_status = True
-                    unblock_channel(channel)
-                else:
-                    channel.workinghours_status = False
-
-                if not channel.workinghours_status and not channel.channel_blocked:  # Block at non working time
-                    block_channel(channel)
-
-                if channel.total_call_time > channel.channel_active_int and channel.channel_status:
-                    block_channel(channel)
-                elif (datetime.now() - channel.channel_ts_last).total_seconds() > channel.channel_block_int and not channel.channel_status:
-                    unblock_channel(channel)
-                #if (datetime.now() - channel.last_downloads).total_seconds() > channel.download_int and not channel.channel_status:
-                #    start_downloading_activity(channel, sites)
-        time.sleep(raspi.asterisk_check_int)
+            # Block route if not workinghours
+            if not channel.workinghours_status and channel.route_status == 'unblocked':
+                block_route(channel)
+            else:
+                if channel.channel_status == 'busy' and channel.route_status == 'unblocked':    # Block route during the conversation, not to make next call immediately
+                    block_route(channel)
+                if channel.channel_status == 'free' and channel.route_status == 'blocked' and channel.last_free_period > channel.idle_period:  # Unblock after randomly assigned idle period
+                    unblock_route(channel)
 
 
-def get_configuation():
-    ''' Gets current controller and cellphones configuration '''
-    pass
+
+        for channel in channels:
+            print(channel.__dict__)
+            try:
+                s.send(json.dumps({'channel_id': channel.id, 'pairing_status': channel.pairing_status, 'channel_status': channel.channel_status}).encode())
+            except Exception as e:
+                logging.error('Could not send status to server, error: '+str(e))
+        
+        time.sleep(raspi.check_int)
 
 
 
@@ -113,7 +128,16 @@ def add_routes(sites):
             logging.info('Added route to %s via wlan' % ip_addr)
 
 
-def check_asterisk_mobile():
+
+def check_workinghours():
+    ''' Check workinghours '''
+    if (channel.workinghours[0] <= int(datetime.now().strftime('%H')) < channel.workinghours[1]):
+        channel.workinghours_status = True
+    else:
+        channel.workinghours_status = False
+
+
+def check_channels():
     ''' Check mobile status from asterisk console '''
     # logging.info('Checking asterisk status')
     output = subprocess.getoutput('asterisk -x "mobile show devices"')
@@ -125,55 +149,55 @@ def check_asterisk_mobile():
     output_spl = output.split('\n')[1:]
     # logging.info(output_spl)
 
-    # Check dialplan
-    dialplan_output = subprocess.getoutput('asterisk -x "dialplan show outgoing"')
-
-
     for idx, line in enumerate(output_spl):
-
-        if 'channel'+str(idx+1) not in dialplan_output:
-            channels[idx+1].channel_blocked = True
-
         try:
             phone_id, mac_addr, group, adapter, connected, state, sms = line.split()
         except:
             continue
-        # if connected == 'No' and channels[idx].pairing_status:
         if connected == 'No':
-            channels[idx].pairing_status = False
-            s.send(json.dumps({'channel_id': idx+1, 'pairing_status': 'unpaired'}).encode())
-            # logging.error('Device %s was unpaired!!' % phone_id)
-        # elif connected == 'Yes' and not channels[idx].pairing_status:
-        elif connected == 'Yes' and not channels[idx].channel_blocked:
-            channels[idx].pairing_status = True
-            s.send(json.dumps({'channel_id': idx+1, 'pairing_status': 'paired'}).encode())
-            if state == 'Busy' and channel.channel_status:
-                channels[idx].total_call_time += (datetime.now() - channel.last_check_ts).total_seconds
-                channels[idx].last_check_ts = datetime.now()
-                s.send(json.dumps({'channel_id': idx+1, 'channel_status': 'in call'}).encode())
-            elif state == 'Free' and channel.channel_status:
-                s.send(json.dumps({'channel_id': idx+1, 'channel_status': 'online'}).encode())
-        print(channels[idx].__dict__)
+            channels[idx].pairing_status = 'not paired'
+        elif connected == 'Yes':
+            channels[idx].pairing_status = 'paired'
 
-    raspi.asterisk_status_last = datetime.now()
+        if state == 'Free':
+            channels[idx].channel_status = 'free'
+            channels[idx].last_free_period += (datetime.now() - raspi.last_check_ts).total_seconds()
+            channels[idx].last_busy_period = 0
+        elif state == 'Busy':
+            channels[idx].channel_status = 'busy'
+            channels[idx].last_free_period = 0 
+            channels[idx].last_busy_period += (datetime.now() - raspi.last_check_ts).total_seconds()
+        elif state == 'None':
+            channels[idx].channel_status = 'offline'
 
 
-def block_channel(channel):
+
+def check_routes():
+    ''' Checks dialplan status '''
+    dialplan_output = subprocess.getoutput('asterisk -x "dialplan show outgoing"')
+    for channel in channels:
+        if channel.channel_name not in dialplan_output:
+            channel.route_status = 'blocked'
+        else:
+            channel.route_status = 'unblocked'
+
+
+
+def block_route(channel):
     os.system('asterisk -rx  "dialplan remove extension _X.@outgoing %s"' % channel.id )
-    channel.channel_status = False
-    logging.info('Channel %s was blocked' % channel.id )
-    s.send(json.dumps({'channel_id': channel.id, 'channel_status': 'blocked'}).encode())
-    channel.channel_blocked = True
-    channel.channel_ts_last = datetime.now()
+    channel.idle_period = random.randint(50,600)  # we need to change this period every time
+    logging.info('Channel %s was blocked' % channel.channel_name )
+    # channel.channel_ts_last = datetime.now()
 
 
-def unblock_channel(channel):
+def unblock_route(channel):
     os.system('asterisk -rx "dialplan add extension _X.,%s,Dial(Mobile/channel%s/${EXTEN},45) into outgoing"' % (channel.id, channel.id))
-    channel.channel_status = True
-    logging.info('Channel %s was unblocked' % channel.id )
-    s.send(json.dumps({'channel_id': channel.id, 'channel_status': 'online'}).encode())
-    channel.channel_blocked = False
-    channel.channel_ts_last = datetime.now()
+    logging.info('Channel %s was unblocked' % channel.channel_name )
+    # channel.channel_ts_last = datetime.now()
+
+
+def check_wlan_status():
+    pass
 
 
 def start_downloading_activity(channel, sites):
